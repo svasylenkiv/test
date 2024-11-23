2151d40157420d3d39156f4f8520d0addf16e6db
Stepan Vasylenkiv <svasylenkiv@gmail.com>
2024-11-23 23:57:49 +0200

    ec2

---
+++ ec2/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  associate_public_ip_address = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-srv"
+    Project = var.project
+    Env     = var.environment
+  }
+
+  user_data = var.user_data
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,31 @@
+variable "project" {
+  description = "Назва проєкту"
+  type        = string
+}
+
+variable "environment" {
+  description = "Середовище (dev, stg, prod)"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI для EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Тип EC2 інстансу"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "subnet_id" {
+  description = "ID сабнету, в якому буде створено інстанс"
+  type        = string
+}
+
+variable "user_data" {
+  description = "Скрипт user_data для інстансу"
+  type        = string
+  default     = ""
+}
