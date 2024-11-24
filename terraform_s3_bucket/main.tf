deb9c318bb950e047d5ef4f95c249c6beb7b99ac
Stepan Vasylenkiv <svasylenkiv@gmail.com>
2024-11-24 09:15:11 +0200

    s3

---
+++ terraform_s3_bucket/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Генерація випадкового рядка
+resource "random_string" "bucket_suffix" {
+  length  = 6  # Довжина випадкового рядка
+  upper   = false
+  lower   = true
+  special = false
+}
+
+# Оголошення бакета з випадковим суфіксом
+resource "aws_s3_bucket" "common_bucket" {
+  bucket = "${var.project}-common-terraform-bucket-${random_string.bucket_suffix.result}"
+  acl    = "private"
+
+  tags = {
+    Name    = "${var.project} Common Terraform Bucket"
+    Project = var.project
+  }
+}
+
+
